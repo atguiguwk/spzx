2da16c626b46c48a3358a69d23c772a379f97af0
wk <wk@atguigu.com>
2023-10-25 09:30:18 +0800

    第一阶段，添加全局异常处理，和自定义异常处理

--- spzx-manager/src/main/java/com/atguigu/spzx/manager/service/impl/SysUserServiceImpl.java
+++ spzx-manager/src/main/java/com/atguigu/spzx/manager/service/impl/SysUserServiceImpl.java
@@ -1,11 +1,13 @@
 package com.atguigu.spzx.manager.service.impl;
 
 import com.alibaba.fastjson.JSON;
+import com.atguigu.spzx.common.exception.GuiguException;
 import com.atguigu.spzx.manager.mapper.SysUserMapper;
 import com.atguigu.spzx.manager.service.SysUserService;
 import com.atguigu.spzx.model.dto.system.LoginDto;
 import com.atguigu.spzx.model.entity.system.SysUser;
 import com.atguigu.spzx.model.entity.user.UserInfo;
+import com.atguigu.spzx.model.vo.common.ResultCodeEnum;
 import com.atguigu.spzx.model.vo.system.LoginVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -32,7 +34,7 @@ public class SysUserServiceImpl implements SysUserService {
         SysUser sysUser = sysUserMapper.selectByUserName(userName);
         //3.如果查不到，返回错误信息
         if (sysUser == null){
-            throw new RuntimeException("用户名不正确");
+            throw new GuiguException(ResultCodeEnum.LOGIN_ERROR);
         }
         //4.如果能查到，用户存在
         //5.获取输入密码，比较密码
@@ -41,7 +43,7 @@ public class SysUserServiceImpl implements SysUserService {
         //把输入的密码加密，在进行比较
         input_Password = DigestUtils.md5DigestAsHex(input_Password.getBytes());
         if (!input_Password.equals(database_Password)){
-            throw new RuntimeException("密码不正确");
+            throw new GuiguException(ResultCodeEnum.LOGIN_ERROR);
         }
         //6.如果一致，成功，否则失败
         //7.登录成功，生成唯一标识token
